13e91d814543b25feb238a3285d4de48b932916b
yuvaraj <shivarajnaidu@gmail.com>
2018-11-14 09:27:09 +0530

    cleanup

--- projects/ng-back-to-top/src/public_api.ts
+++ projects/ng-back-to-top/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng-back-to-top
  */
 
-export * from './lib/ng-back-to-top.service';
 export * from './lib/ng-back-to-top/ng-back-to-top.component';
 export * from './lib/ng-back-to-top.module';
